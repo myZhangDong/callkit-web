d2485f4f36d11272ce2d202b9a84b8ee67953bba
myZhangDong <973849793@qq.com>
2022-04-21 15:37:57 +0800

    project init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Layout from './layout';
+
+function App() {
+	return (
+		<div className="App">
+			{/* <header className="App-header">123</header> */}
+			<Layout></Layout>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/layout/index.tsx
@@ -0,0 +1,6 @@
+import './index.less';
+function Layout() {
+	return <div className="callkit-layout-cantainer">12</div>;
+}
+
+export default Layout;
